ce625013b931cd87e9cfca63b6433f96c951e103
DSU-adbabcock <austin.babcock@trojans.dsu.edu>
2018-06-15 02:53:01 -0500

    deleted old nonworking version

--- steamscraper.py
+++ steamscraper.py
@@ -7,20 +7,20 @@ pages = int(sys.argv[1])
 curPage = 1
 
 for page in range(1, pages+1):
-	page = requests.get("http://store.steampowered.com/search/?specials=1&page=" + str(page))
+	page = requests.get("http://store.steampowered.com/search/?specials=1&page=" + str(page)) #pulls appropriate page for loop iteration
 
 	soup = BeautifulSoup(page.content, 'html.parser')
 
-	results = soup.find(id="search_results")
+	results = soup.find(id="search_results") #subdivides html for easier parsing
 
-	title_tags = results.select('div.search_name span.title')
-	titles = [tt.get_text() for tt in title_tags]
+	title_tags = results.select('div.search_name span.title') #pulls html for titles
+	titles = [tt.get_text() for tt in title_tags] #pulls text from html
 	#titles
 
-	price_tags = results.select('div.search_price strike')
+	price_tags = results.select('div.search_price strike')  #see above
 	prices = [pt.get_text() for pt in price_tags]
 
-	sale_tags = results.select('div.search_discount span')
+	sale_tags = results.select('div.search_discount span') #I think we've caught on by now
 	sales = [st.get_text() for st in sale_tags]
 
 	print("\nPAGE " + str(curPage) + "------------------------------------------------------------------\n")
